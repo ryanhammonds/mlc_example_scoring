5c3bcf368ca2ce082a1176c476dd5a5b50c1f1d3
Ryan Hammonds <ryanphammonds@gmail.com>
2024-05-28 12:03:55 -0700

    enhancer: update to auprc

--- enhancer/predict.py
+++ enhancer/predict.py
@@ -1,11 +1,9 @@
 import pickle
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
-
+from sklearn.metrics import precision_recall_curve, auc
 
 def predict(data_path, pkl_path, ohe=None):
     """Predict enhancer data.
@@ -20,7 +18,7 @@ def predict(data_path, pkl_path, ohe=None):
         the weights of the torch model, "model.pkl"
     ohe : OneHotEncoder
         Used to ensure categories are the same in train/test.
-        Must set handle_unknown='ignore'. Required at test time.
+        Required at test time.
 
     Returns
     -------
@@ -29,64 +27,55 @@ def predict(data_path, pkl_path, ohe=None):
     """
     # Read csv
     df = pd.read_csv(data_path)
-    
+
     # Drop labels/targets
-    X = df.drop(df.columns[10:18], axis=1)
-    
+    df = df.drop(df.columns[10:18], axis=1)
+
     # Preprocessing
     float_columns = ["ABC.Score", "normalizedDNase_enh_squared", "3DContact_squared", "3DContact"]
     X_float = df[float_columns].copy()
     X_float = StandardScaler().fit_transform(X_float)
     df[float_columns] = X_float
-    
+
     if ohe is None:
         ohe = OneHotEncoder(handle_unknown='ignore')
         X = ohe.fit_transform(df)
     else:
         X = ohe.transform(df)
     # ...
-    
+
     # Load sklearn classifier
     with open(pkl_path, 'rb') as f:
         clf = pickle.load(f)
 
-    # Note: your classifier will be score using:
-    y = clf.predict(X)
-    
-    return y, df["chr"], ohe
+    # Note: your classifier will be scored using:
+    # auc = score(X, y, clf, df["chr"])
+    y_proba = clf.predict_proba(X)[:, 1]
 
+    return y_proba, df["chr"], ohe
 
 # Scoring
-def score(y_true, y_pred, chromosomes):
+def score(y_true, y_pred_proba, chromosomes):
     """Enhancer scoring function."""
-    
+
     chromosomes_unique = np.unique(chromosomes)
-    
-    roc_auc = np.zeros(len(chromosomes_unique))
+
+    auprc  = np.zeros(len(chromosomes_unique))
 
     for i, c in enumerate(chromosomes_unique):
-        
+
         mask = chromosomes == c
-        
+
         y_chr_true = y_true[mask]
-        y_chr_pred = y_pred[mask]
+        y_chr_pred_proba = y_pred_proba[mask]
 
         try:
-            roc_auc[i] = roc_auc_score(y_chr_true, y_chr_pred)
+            precision, recall, _ = precision_recall_curve(y_chr_true, y_chr_pred_proba)
+            auprc[i] = auc(recall, precision)
         except:
             # Predicted all zeros
-            roc_auc[i] = 0
-    
-    score = roc_auc.mean()
-    
+            auprc[i] = 0
+
+    score = auprc.mean()
+
     return score
-
-# Example with train
-data_path = "../../enhancer/X_train.csv"
-pkl_path = "model.pkl"
-y_pred, chromosomes, ohe = predict(data_path, pkl_path)
-
-# Example with test (you don't have this data, it's what we will be running)
-y_pred, chromosomes, _ = predict("../../enhancer/X_test.csv", "model.pkl", ohe)
-y_true = pd.read_csv("../../enhancer/y_test.csv")["Regulated"].values.astype(int)
-score = score(y_true, y_pred, chromosomes)
